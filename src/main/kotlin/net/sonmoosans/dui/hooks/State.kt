2721d25c2819f3e13fca4d5f0a82da331890fdd7
SonMooSans <god63820869@gmail.com>
2022-08-27 19:36:48 +0800

    Move State Context

--- src/main/kotlin/net/sonmoosans/dui/hooks/State.kt
+++ src/main/kotlin/net/sonmoosans/dui/hooks/State.kt
@@ -1,5 +1,6 @@
 package net.sonmoosans.dui.hooks
 
+import net.sonmoosans.dui.Data
 import net.sonmoosans.dui.context.RenderContext
 import net.sonmoosans.dui.context.State
 import net.sonmoosans.dui.utils.generateId
@@ -23,3 +24,28 @@ fun<S> RenderContext<*, *>.useState(id: String, initial: S): State<S> {
 }
 
 fun<S: Any, P: Any> RenderContext<P, *>.useState(id: String) = useState<S?>(id, null)
+
+interface StateContext<P> {
+    val data: Data<P>
+
+    val<S> State<S>.current
+        get() = data.states[id]!! as State<S>
+
+    var <S> State<S>.value: S
+        get() = current.raw
+        set(v) {
+            current.raw = v
+        }
+
+    infix fun<S> State<S>.update(updater: S.() -> Unit) {
+        updater(value)
+    }
+
+    operator fun<S> State<S>.timesAssign(value: S) { this.value = value }
+
+    infix fun<S> State<S>.set(value: (prev: S) -> S) {
+        this.value = value(this.value)
+    }
+
+    fun<S> State<S>.asString() = raw.toString()
+}
