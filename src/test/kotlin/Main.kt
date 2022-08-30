84bb679bccfccd9b6f0aa109089f2bd6be6d16b7
SonMooSans <god63820869@gmail.com>
2022-08-30 19:18:05 +0800

    Add Todo App in Example Test

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -7,10 +7,15 @@ import net.sonmoosans.dui.bjda.DUIModule
 import net.sonmoosans.dui.component
 import net.sonmoosans.dui.components.*
 import net.sonmoosans.dui.context.RenderContext
+import net.sonmoosans.dui.graphics.drawStringCenter
+import net.sonmoosans.dui.graphics.paint
+import net.sonmoosans.dui.graphics.toInputStream
 import net.sonmoosans.dui.hooks.sync
 import net.sonmoosans.dui.hooks.useModalLazy
 import net.sonmoosans.dui.hooks.useState
 import net.sonmoosans.dui.utils.*
+import java.awt.Color
+import java.awt.image.BufferedImage
 
 data class Props(override val locale: DiscordLocale): LocaleProps
 
@@ -83,10 +88,64 @@ suspend fun main() {
         }
 
         supercommand(
-            TestCommand()
+            TestCommand(), TodoCommand()
         )
 
         + DUIModule()
+    }
+}
+
+val ModernTodoApp = component<Unit> {
+    val todos = useState { mutableListOf("hello") }
+
+    var expectedHeight = (110 * todos.value.size) + (50 * 2)
+
+    if (todos.size != 0) {
+        expectedHeight -= 10
+    }
+
+    val image = BufferedImage(500, expectedHeight, BufferedImage.TYPE_INT_RGB)
+
+    val addModal by useModalLazy {
+        title = "Add Todo"
+        row {
+            input("todo", "Todo Content")
+        }
+        submit {
+            todos += event["todo"]
+
+            event.edit()
+        }
+    }
+
+    with (image.createGraphics()) {
+        val (w, h) = 450 to 100
+
+        font = font.deriveFont(25f)
+        translate((500 - w) / 2, 50)
+
+        todos.forEach { todo ->
+
+            paint(Color.DARK_GRAY) {
+                fillRoundRect(0, 0, w, h, 20, 20)
+            }
+
+            paint(Color.WHITE) {
+                drawStringCenter(todo, w, h)
+            }
+
+            translate(0, h + 10)
+        }
+    }
+
+    row {
+        button("Add") {
+            event.replyModal(addModal).queue()
+        }
+    }
+
+    files {
+        file("ui.png", image.toInputStream())
     }
 }
 
@@ -96,6 +155,14 @@ fun TestCommand() = command("test", "Testing Command") {
         val ui = example.create(event.user.idLong, Props(locale = event.userLocale)) {
             sync(event.hook)
         }
+
+        event.reply(ui).queue()
+    }
+}
+
+fun TodoCommand() = command("todo", "Todo App") {
+    execute {
+        val ui = ModernTodoApp.create(event.user.idLong, Unit)
 
         event.reply(ui).queue()
     }
