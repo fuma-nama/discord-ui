3238d6c91ef35a6bf14e5984efd8eafb26e6cc81
SonMooSans <god63820869@gmail.com>
2022-08-31 21:11:55 +0800

    Adds OnceComponent

---
+++ src/main/kotlin/net/sonmoosans/dui/OnceComponent.kt
@@ -0,0 +1,35 @@
+package net.sonmoosans.dui
+
+import net.dv8tion.jda.api.utils.messages.MessageCreateData
+import net.dv8tion.jda.api.utils.messages.MessageEditData
+import net.sonmoosans.dui.context.RenderContext
+
+fun<P: Any> once(render: RenderContext<P, *>.() -> Unit) = OnceComponent(render)
+
+/**
+ * Similar to SinceDataComponent, but data won't be stored at every render
+ *
+ * Listeners won't work since no data is stored
+ */
+class OnceComponent<P: Any>(override val render: RenderContext<P, *>.() -> Unit) : AbstractComponent<P>() {
+    override fun getData(id: Long): Data<P>? {
+        return null
+    }
+
+    fun create(props: P) = render(createData(props))
+    fun edit(props: P) = edit(createData(props))
+
+    fun createWithData(props: P): Pair<Data<P>, MessageCreateData> {
+        val data = createData(props)
+        return data to render(data)
+    }
+
+    fun editWithData(props: P): Pair<Data<P>, MessageEditData> {
+        val data = createData(props)
+        return data to edit(data)
+    }
+
+    private fun createData(props: P) = Data(0, props)
+
+    override fun destroy(data: Data<P>) {}
+}
